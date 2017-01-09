66069f7799a14f85ed5894cfa571d1bfbecea825
Eunju <Eunju@218.38.137.28>
2017-01-09 15:46:54 +0900

    Paint

---
+++ src/Paint/PaintApp.java
@@ -0,0 +1,15 @@
+package Paint;
+
+public class PaintApp {
+
+	public static void main(String[] args) {
+		
+		Point point1=new Point();
+		point1.setX(100);
+		point1.setY(200);
+		
+		point1.show();
+		
+	}
+
+}
